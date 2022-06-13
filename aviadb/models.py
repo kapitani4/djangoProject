dd7998920042616d1b5fa75004a9ec22771f417f
kapitani4 <swapsap@mail.ru>
2022-06-13 23:31:10 +0300

    release 1.0.4 добавлены поля для хранения файлов

--- aviadb/models.py
+++ aviadb/models.py
@@ -36,12 +36,16 @@ class Drawing(models.Model):
     ###Таблица с чертежами###
 
     plan = models.CharField(max_length=200, help_text="Чертеж")
+    #planFile = models.FileField(upload_to='files')
     equipment = models.CharField(max_length=200, help_text="Оснастка")
+    #equipmentFile = models.FileField(upload_to='files')
     material = models.CharField(max_length=200, help_text="Материал")
     square = models.CharField(max_length=200, help_text="Общая площадь")
     weight = models.CharField(max_length=200, help_text="Масса")
     diamino = models.CharField(max_length=200, help_text="Diamino")
+    #diaminoFile = models.FileField(upload_to='files')
     laser3d = models.CharField(max_length=200, help_text="Laser3D")
+    #laser3dFile = models.FileField(upload_to='files')
     revision = models.CharField(max_length=200, help_text="Ревизия")
     samplesAreWitnesses = models.CharField(max_length=200, help_text="Образцы свидетели")
     executor = models.CharField(max_length=200, help_text="Испольнитель")
