8f11d4913657cbd584634aec0c1e298318300d5e
kapitani4 <swapsap@mail.ru>
2022-06-13 12:57:01 +0300

    release 1.0.3 изменены адреса для редиректа по запросам

--- aviadb/urls.py
+++ aviadb/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^about', views.about, name='about'),
-    re_path(r'^compartments/(?P<air_id>[0-9])/', views.compartments, name='compartments'),
-    re_path(r'^drawing/(?P<compartments_id>[0-9])/', views.drawing, name='drawing'),
-    re_path(r'^plan/(?P<drawing_id>[0-9])/', views.plan, name='plan'),
+    re_path(r'^compartments/(?P<air_id>[0-9]{1,3})/', views.compartments, name='compartments'),
+    re_path(r'^drawing/(?P<compartments_id>[0-9]{1,3})/', views.drawing, name='drawing'),
+    re_path(r'^plan/(?P<drawing_id>[0-9]{1,3})/', views.plan, name='plan'),
     path('', views.index, name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
